f13eb7d03ac4ab29df4e45aa62661499efc6a1c7
yuichi-rice <71434566+yuichi-rice@users.noreply.github.com>
2021-05-03 16:06:35 +0900

    全てテストパス

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,5 +5,12 @@ FactoryBot.define do
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
+
+    trait :complete_task do
+      status { 'done' }
+      completion_date { Time.current.yesterday }
+    end
+
+    association :project
   end
 end
