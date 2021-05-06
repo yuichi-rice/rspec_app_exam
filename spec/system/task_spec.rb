5a5edf3d8928b986fd7e1c40c9416dfcbbc1ba7e
yuichi-rice <71434566+yuichi-rice@users.noreply.github.com>
2021-05-06 21:18:49 +0900

    fix_task_spec_delete_project

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project) { (create(:project) )}
-  let(:task) { create(:task, project: project) }  
+  let(:task) { create(:task) }  
 
   describe 'Task一覧' do
     context '正常系' do
